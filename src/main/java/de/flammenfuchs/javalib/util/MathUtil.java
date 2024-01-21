7a18500323a6ceef88a4a7f09240a8ffa93d1db1
FlorianWip <florianwippert@gmx.de>
2024-01-21 17:31:19 +0100

    :sparkles: Bump to 2.3.0-SNAPSHOT, Added MathUtil.isInRange(...)

--- src/main/java/de/flammenfuchs/javalib/util/MathUtil.java
+++ src/main/java/de/flammenfuchs/javalib/util/MathUtil.java
@@ -1,5 +1,7 @@
 package de.flammenfuchs.javalib.util;
 
+import lombok.val;
+
 /**
  * A util for calculations, math etc.
  */
@@ -27,5 +29,83 @@ public class MathUtil {
     public static float round(float d, int digits) {
         double factor = java.lang.Math.pow(10, digits);
         return (float) (java.lang.Math.round(d * factor) / factor);
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(byte val, byte min, byte max) {
+        return val >= min && val <= max;
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(short val, short min, short max) {
+        return val >= min && val <= max;
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(int val, int min, int max) {
+        return val >= min && val <= max;
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(long val, long min, long max) {
+        return val >= min && val <= max;
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(float val, float min, float max) {
+        return val >= min && val <= max;
+    }
+
+    /**
+     * Check if value is in range
+     *
+     *
+     * @param val value to check
+     * @param min minimal value
+     * @param max maximum value
+     * @return value in or not in range (min/max included)
+     */
+    public static boolean isInRange(double val, double min, double max) {
+        return val >= min && val <= max;
     }
 }
